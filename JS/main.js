d4b90451e3b31977b87aea2f035f00a3a4760570
中田佳孝 <a1086yn5974@gmail.com>
2021-06-20 14:09:49 +0900

    first-commit

---
+++ JS/main.js
@@ -0,0 +1,28 @@
+//配列の問題
+const array = [2, 4, 7, 5, 4, 3, 8];
+const newarray = array.filter( function( value, index, array ) {
+  return array.indexOf(value) === index;
+})
+console.log(newarray);
+
+function LeapYear(year){
+  if( (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+    return true;
+  }
+  return false;
+}
+
+//うるう年の問題
+let checkYear2020 = 2020;
+if(LeapYear(checkYear2020)){
+  console.log(checkYear2020 + '年はうるう年です。');
+}else{
+  console.log(checkYear2020 + '年はうるう年ではありません。');
+}
+
+let checkYear2021 = 2021;
+if(LeapYear(checkYear2021)){
+  console.log(checkYear2021 + '年はうるう年です。');
+}else{
+  console.log(checkYear2021 + '年はうるう年ではありません。');
+}
